831252b48d6ecd559d607ba752117bc0f2e63d84
Chad Chaney <cchane4@gmail.com>
2019-08-31 16:55:12 -0500

    adding logic for clicking right and wrong colors

--- color_game.js
+++ color_game.js
@@ -9,7 +9,26 @@ let colors = [
     "rgb(255, 0, 255)"
 ] 
 
-let squares = document.querySelectorAll(".square"); 
+let squares = document.querySelectorAll(".square");
+let picked_color = colors[3]; 
+let color_display = document.getElementById("color_display");
+
+color_display.textContent = picked_color; 
+
 for (let i = 0; i < squares.length; i++){ 
+    // add initial colors to squares 
     squares[i].style.backgroundColor = colors[i]; 
+
+    // add click listeners to all squares 
+    squares[i].addEventListener("click", function(){ 
+    // grab color of clicked square 
+      let clicked_color = this.style.backgroundColor; 
+        if (clicked_color === picked_color){ 
+          alert("you are correct"); 
+        } else { 
+          alert('you are wrong'); 
+        }
+    })
 }
+
+
