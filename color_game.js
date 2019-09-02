8558e792a1f2fdd8ebf7448058a121aed227c193
Chad Chaney <cchane4@gmail.com>
2019-09-02 00:03:22 -0500

    working on styling

--- color_game.js
+++ color_game.js
@@ -1,5 +1,4 @@
 
-
 let num_squares = 6; 
 let colors = generate_random_colors (num_squares); 
 let squares = document.querySelectorAll(".square");
@@ -50,7 +49,8 @@ reset_button.addEventListener("click", function(){
   //change colors of squares on the page 
   for (let i = 0; i < squares.length; i++){ 
     squares[i].style.backgroundColor = colors[i];
-    h1.style.background = "#232323";  
+    h1.style.backgroundColor = "steelblue";
+    message_display.textContent = "";   
   }
 }); 
 
@@ -70,7 +70,7 @@ for (let i = 0; i < squares.length; i++){
           change_colors(clicked_color);  
           h1.style.backgroundColor = clicked_color; 
         } else { 
-        this.style.backgroundColor =  "#232323" ; 
+        this.style.backgroundColor =  "#232323"; 
         message_display.textContent = "Try Again"; 
          }
     })
@@ -91,30 +91,21 @@ function pick_color(){
   of the arrary. even though the length of the array is 1 more than the highest index
   so the length is 6 but highest number we want is 5, but math.floor * math.random 
   assures we only choose indexes between 0 and 5 so we are good */
-
   let random = Math.floor(Math.random() * colors.length); 
   return colors[random]; 
 }
 
 function generate_random_colors (num){ 
-//make array 
 let arr = []; 
-//repeat num times
 for (let i = 0; i < num; i++){ 
-  // get random color and push into array
   arr.push(random_color()); 
 }
-//add num to array 
-//return the arry 
 return arr; 
 }
 
 function random_color (){ 
-//pick a red from 0-255
 let r = Math.floor(Math.random() * 256);  
-//pick a blue from 0-255
 let g = Math.floor(Math.random() * 256);  
-//pick a green from 0-255
 let b  = Math.floor(Math.random() * 256); 
 return "rgb(" + r + ", " + g + ", " + b + ")";
 }
